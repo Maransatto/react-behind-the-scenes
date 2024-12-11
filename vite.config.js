73dcf9c2956884264511423d7aeeca883d82a0f5
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-12-10 22:27:22 -0300

    feat: 216. optimizing react with MillionJS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import MillionLint from "@million/lint";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [MillionLint.vite({ auto: true }), react()],
+});
